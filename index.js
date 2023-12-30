d834bc57fd8f33a6d76adf4aea540e8759ee0f39
shlegeldavid <shlegeldavid@gmail.com>
2023-12-30 20:53:21 +0300

    AA

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
-export const concatStrings = (firstString, secondString) => {
-    return (firstString + secondString);
-}
+export const concatStrings = (firstString, secondString) => (firstString + secondString);
 
 export const isString = (str) => {
-    if (typeof str === 'string') {
-        return true;
-    } else {
-        return false;
-    }
-}
+  if (typeof str === 'string') {
+    return true;
+  }
+  return false;
+};
 
 export const identifySign = (number) => {
-    if (number === 0) {
-        return 'Ноль';
-    } else if (number > 0) {
-        return 'Положительное число';
-    } else {
-        return 'Отрицательное число';
-    }
-}
+  if (number === 0) {
+    return 'Ноль';
+  } if (number > 0) {
+    return 'Положительное число';
+  }
+  return 'Отрицательное число';
+};
 
 export const reverseWords = (str) => str.split(' ').reverse().join(' ');
-   
+
 export function wordsCount(str) {
-    return str.split(' ').length;
+  return str.split(' ').length;
 }
-
-
